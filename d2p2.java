ac80c1b7d24bb07e4310963149cca3364f02da61
ANotFox <adityaraghuram90@gmail.com>
2024-12-02 16:27:00 +0530

    Attempted to improve D2P2 | Looks more organised, runs with more time V__V

--- d2p2.java
+++ d2p2.java
@@ -15,65 +15,55 @@ import java.util.ArrayList;
 
 
 public class d2p2 {
-    static String filename = "data/d2.txt";
+    static final String filename = "data/d2.txt";
 
+    static boolean removeelementandsendtocheck(List<Integer> levels, int i) {
+        List<Integer> removeleft = new ArrayList<>(levels);
+        List<Integer> removeright = new ArrayList<>(levels);
+        removeleft.remove(i);
+        removeright.remove(i + 1);
+
+        boolean x = subcheck(removeleft);
+        boolean y = subcheck(removeright);
+        // System.out.println("Checking left " + removeleft + " " + x);
+        // System.out.println("Checking right " + removeright + " " + y);
+
+        // System.out.println(levels + " is out of updates");
+        if (x || y) { return true; } else { return false; }
+    }
     static boolean subcheck(List<Integer>levels) {
         int change, prevchange = 0;
-
         prevchange = levels.get(1) - levels.get(0);
-                if (Math.abs(prevchange) > 3 || Math.abs(prevchange) == 0) {
-                    // System.out.println("Difference is " + change);
-                    return false;
-                }
         
-                for (int i = 1; i < levels.size() - 1; i++) {
-                    change = levels.get(i + 1) - levels.get(i);
+        for (int i = 0; i < levels.size() - 1; i++) {
+            change = levels.get(i + 1) - levels.get(i);
 
-                    if (prevchange * change < 0) {
-                        // System.out.println("Sign difference is " + prevchange + "*" + change);
-                        return false;
-                    }
+            if (prevchange * change < 0 || Math.abs(change) > 3 || Math.abs(change) == 0) { 
+                // System.out.println("Sign difference and deltas are " + prevchange + "*" + change);
+                return false;
+            }
 
-                    if (Math.abs(change) > 3 || Math.abs(change) == 0) {
-                        // System.out.println("Difference is " + change);
-                        return false;
-                    }
-
-                    prevchange = change;
-                }
+            prevchange = change;
+        }
 
         return true;
     }
+    
     static boolean check(List<Integer> levels) {
         int change, prevchange = 0;
-        int update = 0;
+        // int update = 0;
         boolean safe = true;
 
         prevchange = levels.get(1) - levels.get(0);
         if (Math.abs(prevchange) > 3 || Math.abs(prevchange) == 0) {
             // System.out.println("Difference is " + change);
-            safe = false;
-            List<Integer> removeleft = new ArrayList<>(levels);
-            List<Integer> removeright = new ArrayList<>(levels);
-            removeleft.remove(0);
-            removeright.remove(1);
-
-            boolean x = subcheck(removeleft);
-            // System.out.println("Checking left " + removeleft + " " + x);
-            boolean y = subcheck(removeright);
-            // System.out.println("Checking right " + removeright + " " + y);
-
-            update = 0; //out of updates
-            // System.out.println(levels + " is out of updates at prevchange checkpoint");
-            if (x || y) { return true; } else { return false; }
-        }
+            safe = removeelementandsendtocheck(levels, 0);
+            }
 
         for (int i = 1; i < levels.size() - 1; i++) {
             change = levels.get(i + 1) - levels.get(i);
 
             if (prevchange * change < 0) {
-                // System.out.println("Sign difference is " + prevchange + "*" + change);
-                if (update == 0) {
                     List<Integer> removeleft = new ArrayList<>(levels);
                     List<Integer> removecentre = new ArrayList<>(levels);
                     List<Integer> removeright = new ArrayList<>(levels);
@@ -84,46 +74,21 @@ public class d2p2 {
                     boolean xx = subcheck(removeleft);
                     // System.out.println("Checking left " + removeleft + " " + xx);
                     boolean zz = subcheck(removecentre);
+                    // System.out.println("Checking centre " + removecentre + " " + zz);
                     boolean yy = subcheck(removeright);
                     // System.out.println("Checking right " + removeright + " " + yy);
-
-                    update = 1;
                     if (xx || yy || zz) { return true; } else { return false; }
                     
                     // System.out.println(levels + " is out of updates at sign checkpoint " + safe);
-                    // break;
-                }
-                else { safe = false;
-                // System.out.println(levels + " is now " + safe + " for " + levels.get(i) + " " + prevchange + " " + change);
-                        break;}
             }
 
             if (Math.abs(change) > 3 || Math.abs(change) == 0) {
-                // System.out.println("Difference is " + change);
-
-                if (update == 0) {
-                    List<Integer> removeleft = new ArrayList<>(levels);
-                    List<Integer> removeright = new ArrayList<>(levels);
-                    removeleft.remove(i);
-                    removeright.remove(i + 1);
-
-                    boolean x = subcheck(removeleft);
-                    // System.out.println("Checking left " + removeleft + " " + x);
-                    boolean y = subcheck(removeright);
-                    // System.out.println("Checking right " + removeright + " " + y);
-                    update = 1;
-
-                    if (x || y) { return true; } else { return false; }
-                    
-                    // System.out.println(levels + " is out of updates");
-                    // break;
-                }
-                else { safe = false; break;}
+                   safe = removeelementandsendtocheck(levels, i); 
             }
 
             prevchange = change;
         }
-        // System.out.println("List is " + levels + " " + safe + "\n");
+        
         return safe;
     }
 
@@ -140,9 +105,9 @@ public class d2p2 {
                 }
 
                 if (check(levels)) { count++; 
-                    System.out.println("List is safe" + levels + "\n");
+                    // System.out.println("List is safe" + levels + "\n");
                 }
-                else {System.out.println("List is not safe" + levels + "");}
+                // else {System.out.println("List is not safe" + levels + "");}
 
             }
         } catch (IOException e) { e.printStackTrace(); }
